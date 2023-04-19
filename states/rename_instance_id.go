ba075ed5795f4d4f049bb26952a4c7194f7b3ade
jaime <yun.zhang@zilliz.com>
2023-04-19 11:56:04 +0800

    Support instance id for kafka mq type
    Signed-off-by: jaime <yun.zhang@zilliz.com>

---
+++ states/rename_instance_id.go
@@ -0,0 +1,475 @@
+//go:build WKAFKA
+// +build WKAFKA
+
+package states
+
+import (
+	"context"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"path"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/apache/pulsar-client-go/pulsar"
+	"github.com/golang/protobuf/proto"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.uber.org/zap"
+
+	"github.com/milvus-io/birdwatcher/mq/kafka"
+	mypulsar "github.com/milvus-io/birdwatcher/mq/pulsar"
+	"github.com/milvus-io/birdwatcher/proto/v2.2/commonpb"
+	"github.com/milvus-io/birdwatcher/proto/v2.2/datapb"
+	etcdpbv2 "github.com/milvus-io/birdwatcher/proto/v2.2/etcdpb"
+	"github.com/milvus-io/birdwatcher/proto/v2.2/internalpb"
+	"github.com/milvus-io/birdwatcher/states/etcd/common"
+)
+
+var (
+	channelWatch   = "channelwatch"
+	channelRemoval = "datacoord-meta/channel-removal"
+	channelCP      = "datacoord-meta/channel-cp"
+	segmentPrefix  = "datacoord-meta/s"
+)
+
+// RenameInstanceID Only support Kafka MQ
+// Usage:
+// ./bin/birdwatcher --setPChanPrefix --addr 10.15.29.220:2379 --rootPath in01-6b61b7649908674 --newPChanPrefix  in01-fbf9815060cce85
+func RenameInstanceID(addr, mqType, rootPath, newPChanNamePrefix string) error {
+	if len(addr) == 0 {
+		return errors.New("etcd address is empty")
+	}
+	if len(rootPath) == 0 {
+		return errors.New("etcd rootpath is empty")
+	}
+
+	if len(newPChanNamePrefix) == 0 {
+		return errors.New("newPChanPrefix is empty")
+	}
+
+	client, err := getETCDClient(&InstanceInfo{
+		Addr:     addr,
+		RootPath: rootPath,
+	})
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	fmt.Println("Using meta path:", fmt.Sprintf("%s/%s/", rootPath, metaPath))
+	basePath := path.Join(rootPath, metaPath)
+
+	if err = replacePChanPrefixWithinSchema(client, basePath, newPChanNamePrefix); err != nil {
+		return err
+	}
+
+	if err = resetChannelCheckPoint(client, basePath, newPChanNamePrefix, mqType); err != nil {
+		return err
+	}
+
+	if err = resetChannelWatch(client, basePath, newPChanNamePrefix); err != nil {
+		return err
+	}
+
+	if err = removeRemovalChannel(client, basePath); err != nil {
+		return err
+	}
+
+	if err = resetSegmentCheckpoint(client, basePath, newPChanNamePrefix, mqType); err != nil {
+		return err
+	}
+
+	if err = renameRemainedKeys(client, rootPath, newPChanNamePrefix); err != nil {
+		return err
+	}
+	return nil
+}
+
+func getETCDClient(instance *InstanceInfo) (*clientv3.Client, error) {
+	etcdCli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{instance.Addr},
+		DialTimeout: time.Second * 60,
+
+		// disable grpc logging
+		Logger: zap.NewNop(),
+	})
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+
+	rootPath := instance.RootPath
+	// ping etcd
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*120)
+	defer cancel()
+	err = pingEtcd(ctx, etcdCli, rootPath, metaPath)
+	if err != nil {
+		if errors.Is(err, ErrNotMilvsuRootPath) {
+			etcdCli.Close()
+			fmt.Printf("Connection established, but %s, please check your config or use Dry mode\n", err.Error())
+			return nil, err
+		}
+		fmt.Println("cannot connect to etcd with addr:", instance.Addr, err.Error())
+		return nil, err
+	}
+
+	return etcdCli, nil
+}
+
+func listCollections(cli clientv3.KV, basePath string, metaPath string) ([]string, []etcdpbv2.CollectionInfo, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*300)
+	defer cancel()
+	colls, keys, err := common.ListProtoObjectsAdv(ctx, cli,
+		path.Join(basePath, metaPath),
+		func(_ string, value []byte) bool {
+			return true
+		},
+		func(info *etcdpbv2.CollectionInfo) bool {
+			return true
+		})
+
+	snapshotColls, snapshotKeys, err := common.ListProtoObjectsAdv(ctx, cli,
+		path.Join(basePath, path.Join("snapshots", metaPath)),
+		func(_ string, value []byte) bool {
+			return true
+		},
+		func(info *etcdpbv2.CollectionInfo) bool {
+			return true
+		})
+
+	keys = append(keys, snapshotKeys...)
+	colls = append(colls, snapshotColls...)
+	return keys, colls, err
+}
+
+func resetChannelWatch(cli clientv3.KV, basePath string, newChanPrefix string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*120)
+	defer cancel()
+	resp, err := cli.Get(ctx, path.Join(basePath, channelWatch), clientv3.WithPrefix())
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("== update channel-watch key size:", len(resp.Kvs))
+	for _, kv := range resp.Kvs {
+		key := string(kv.Key)
+		cw := &datapb.ChannelWatchInfo{}
+		if err := proto.Unmarshal(kv.Value, cw); err != nil {
+			return err
+		}
+
+		vChannelName := replace(cw.Vchan.ChannelName, newChanPrefix)
+		vChanInfo := &datapb.VchannelInfo{
+			ChannelName:  vChannelName,
+			CollectionID: cw.Vchan.CollectionID,
+		}
+
+		cw.Vchan = vChanInfo
+		mv, err := proto.Marshal(cw)
+		if err != nil {
+			return fmt.Errorf("failed to marshal VchannelInfo: %s", err.Error())
+		}
+
+		err = updateKeyValue(ctx, cli, key, newChanPrefix, string(mv))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func removeRemovalChannel(cli clientv3.KV, basePath string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*120)
+	defer cancel()
+	resp, err := cli.Get(ctx, path.Join(basePath, channelRemoval), clientv3.WithPrefix())
+	if err != nil {
+		return err
+	}
+	fmt.Println("== remove channel-removal key size:", len(resp.Kvs))
+	for _, kv := range resp.Kvs {
+		key := string(kv.Key)
+		_, err = cli.Delete(ctx, key)
+		if err != nil {
+			fmt.Printf("delete key:%s fails\n", key)
+			return err
+		}
+		fmt.Printf("delete key:%s successfully\n", key)
+	}
+
+	return nil
+}
+
+func resetChannelCheckPoint(cli clientv3.KV, basePath string, newChanPrefix string, mqType string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*120)
+	defer cancel()
+	resp, err := cli.Get(ctx, path.Join(basePath, channelCP), clientv3.WithPrefix())
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("== update channel-cp key size:", len(resp.Kvs))
+	for _, kv := range resp.Kvs {
+		key := string(kv.Key)
+		info := &internalpb.MsgPosition{}
+		err = proto.Unmarshal(kv.Value, info)
+		if err != nil {
+			continue
+		}
+
+		replacePosition(info, newChanPrefix, mqType)
+		mv, err := proto.Marshal(info)
+		if err != nil {
+			return fmt.Errorf("failed to marshal msg position info: %s", err.Error())
+		}
+
+		err = updateKeyValue(ctx, cli, key, newChanPrefix, string(mv))
+		if err != nil {
+			return err
+		}
+		fmt.Printf("delete key:%s successfully\n", key)
+	}
+
+	return nil
+}
+
+func serializeRmqID(messageID int64) []byte {
+	b := make([]byte, 8)
+	binary.LittleEndian.PutUint64(b, uint64(messageID))
+	return b
+}
+
+func replacePosition(info *internalpb.MsgPosition, newChanPrefix string, mqType string) {
+	if info == nil {
+		return
+	}
+
+	switch mqType {
+	case "kafka":
+		info.MsgID = kafka.SerializeKafkaID(0)
+	case "pulsar":
+		info.MsgID = mypulsar.SerializePulsarMsgID(pulsar.EarliestMessageID())
+	case "rockmq":
+		info.MsgID = serializeRmqID(0)
+	default:
+		panic("invalid my type:" + mqType)
+	}
+
+	info.Timestamp = getCurrentTime()
+	info.ChannelName = replace(info.ChannelName, newChanPrefix)
+}
+
+func replace(source, newChanPrefix string) string {
+	re := regexp.MustCompile(`(?m)(in01-[0-9a-zA-Z]+)`)
+	for _, match := range re.FindAllString(source, -1) {
+		source = strings.ReplaceAll(source, match, newChanPrefix)
+	}
+	return source
+}
+
+// composeTS returns a timestamp composed of physical part and logical part
+func composeTS(physical, logical int64) uint64 {
+	return uint64((physical << logicalBits) + logical)
+}
+
+// composeTSByTime returns a timestamp composed of physical time.Time and logical time
+func composeTSByTime(physical time.Time, logical int64) uint64 {
+	return composeTS(physical.UnixNano()/int64(time.Millisecond), logical)
+}
+
+// getCurrentTime returns the future timestamp
+func getCurrentTime() uint64 {
+	return composeTSByTime(time.Now(), 0)
+}
+
+func resetSegmentCheckpoint(cli clientv3.KV, basePath string, newChanPrefix string, mqType string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*900)
+	defer cancel()
+	resp, err := cli.Get(ctx, path.Join(basePath, segmentPrefix), clientv3.WithPrefix())
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("== update segments key size:", len(resp.Kvs))
+	for _, kv := range resp.Kvs {
+		key := string(kv.Key)
+		if strings.Contains(key, "statslog") {
+			err = updateKeyValue(ctx, cli, key, newChanPrefix, string(kv.Value))
+			if err != nil {
+				return err
+			}
+			continue
+		}
+
+		info := &datapb.SegmentInfo{}
+		err = proto.Unmarshal(kv.Value, info)
+		if err != nil {
+			continue
+		}
+
+		replacePosition(info.StartPosition, newChanPrefix, mqType)
+		replacePosition(info.DmlPosition, newChanPrefix, mqType)
+		info.InsertChannel = replace(info.InsertChannel, newChanPrefix)
+
+		mv, err := proto.Marshal(info)
+		if err != nil {
+			return fmt.Errorf("failed to marshal segment info: %s", err.Error())
+		}
+
+		err = updateKeyValue(ctx, cli, key, newChanPrefix, string(mv))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func replacePChanPrefixWithinSchema(cli clientv3.KV, basePath, newChanPrefix string) error {
+	keys0, values0, err0 := listCollections(cli, basePath, common.CollectionMetaPrefix)
+	if err0 != nil {
+		return err0
+	}
+
+	keys1, values1, err1 := listCollections(cli, basePath, common.CollectionInfoMetaPrefix)
+	if err1 != nil {
+		return err1
+	}
+
+	keys := make([]string, 0)
+	keys = append(keys, keys0...)
+	keys = append(keys, keys1...)
+
+	values := make([]etcdpbv2.CollectionInfo, 0)
+	values = append(values, values0...)
+	values = append(values, values1...)
+
+	if len(keys) != len(values) {
+		return fmt.Errorf("keys size:%d is not equal value size:%d", len(keys), len(values))
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*300)
+	defer cancel()
+
+	fmt.Println("== update channel prefix of schema key size:", len(keys))
+	for i, k := range keys {
+		v := values[i]
+		newPChannels := make([]string, 0, len(v.PhysicalChannelNames))
+		newVChannels := make([]string, 0, len(v.VirtualChannelNames))
+
+		for _, e := range v.PhysicalChannelNames {
+			pchan := replace(e, newChanPrefix)
+			newPChannels = append(newPChannels, pchan)
+		}
+
+		for _, e := range v.VirtualChannelNames {
+			vchan := replace(e, newChanPrefix)
+			newVChannels = append(newVChannels, vchan)
+		}
+
+		if len(newPChannels) != 0 {
+			fmt.Printf("collection key:%s, old pchannels:%s replace to new pchannels:%s\n",
+				k, strings.Join(v.PhysicalChannelNames, ", "), strings.Join(newPChannels, ", "))
+			v.PhysicalChannelNames = newPChannels
+		}
+
+		if len(newVChannels) != 0 {
+			fmt.Printf("collection key:%s, old vchannels:%s replace to new vchannels:%s\n",
+				k, strings.Join(v.VirtualChannelNames, ", "), strings.Join(newVChannels, ", "))
+			v.VirtualChannelNames = newVChannels
+		}
+
+		sp := make(map[string][]byte)
+		for _, k := range v.PhysicalChannelNames {
+			sp[k] = kafka.SerializeKafkaID(0)
+		}
+
+		v.StartPositions = toKeyDataPairs(sp)
+		mv, err := proto.Marshal(&v)
+		if err != nil {
+			return fmt.Errorf("failed to marshal collection info: %s", err.Error())
+		}
+
+		err = updateKeyValue(ctx, cli, k, newChanPrefix, string(mv))
+		if err != nil {
+			fmt.Printf("collection key:%s, update value fails\n", k)
+			return err
+		}
+	}
+
+	fmt.Println("replace channel prefix and reset start position successfully")
+	return nil
+}
+
+func updateKeyValue(ctx context.Context, cli clientv3.KV, key, newChanPrefix, value string) error {
+	newKey := replace(key, newChanPrefix)
+	_, err := cli.Put(ctx, newKey, value)
+	if err != nil {
+		fmt.Printf("rename key:%s, update value fails\n", key)
+		return err
+	}
+
+	_, err = cli.Delete(ctx, key)
+	if err != nil {
+		fmt.Printf("delete key:%s fails\n", key)
+		return err
+	}
+	fmt.Printf("rename key:%s done\n", key)
+	return nil
+}
+
+func renameRemainedKeys(cli clientv3.KV, basePath string, newChanPrefix string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*15)
+	defer cancel()
+
+	batch := int64(500)
+	opts := []clientv3.OpOption{
+		clientv3.WithSort(clientv3.SortByKey, clientv3.SortAscend),
+		clientv3.WithLimit(batch),
+		clientv3.WithRange(clientv3.GetPrefixRangeEnd(basePath)),
+	}
+
+	fmt.Println("== rename remained keys:")
+	key := basePath
+	for {
+		resp, err := cli.Get(ctx, key, opts...)
+		if err != nil {
+			return err
+		}
+
+		for _, kv := range resp.Kvs {
+			k := string(kv.Key)
+			if strings.Contains(k, channelWatch) ||
+				strings.Contains(k, channelRemoval) ||
+				strings.Contains(k, channelCP) ||
+				strings.Contains(k, segmentPrefix) ||
+				strings.Contains(k, common.CollectionMetaPrefix) ||
+				strings.Contains(k, common.CollectionInfoMetaPrefix) {
+				continue
+			}
+
+			err := updateKeyValue(ctx, cli, k, newChanPrefix, string(kv.Value))
+			if err != nil {
+				return err
+			}
+		}
+
+		if !resp.More {
+			break
+		}
+		// move to next key
+		key = string(append(resp.Kvs[len(resp.Kvs)-1].Key, 0))
+	}
+	return nil
+}
+
+func toKeyDataPairs(m map[string][]byte) []*commonpb.KeyDataPair {
+	ret := make([]*commonpb.KeyDataPair, 0, len(m))
+	for k, data := range m {
+		ret = append(ret, &commonpb.KeyDataPair{
+			Key:  k,
+			Data: data,
+		})
+	}
+	return ret
+}
